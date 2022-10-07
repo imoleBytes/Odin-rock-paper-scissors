ab21ecf830ee25f1bebd4fba8d1f9eec73b7c262
imole <dremkay71@gmail.com>
2022-10-07 18:08:42 +0100

    Game loop and Score functionality added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-console.log("Hello game!")
-
+// console.log("Hello game!")
 
 function getComputerChoice(){
     // console.log(nme)
@@ -10,8 +9,11 @@ function getComputerChoice(){
     return choices[i]
 }
 
+let userScore = 0;
+let comptScore = 0;
+
 function gameRound(){
-    let userChoice = prompt("Choose: (rock | paper | scissors): ")
+    let userChoice = prompt("Choose: (rock | paper | scissors): ").toLowerCase()
     let computerChoice = getComputerChoice()
 
     if (computerChoice==userChoice){
@@ -21,24 +23,40 @@ function gameRound(){
     if (userChoice=="rock"){
         
         if (computerChoice=="scissors"){
+            userScore+=1;
             return "Congrats! You Won., Rock Beat Scissors"
         }else{
+            comptScore+=1;
             return `SORRY! You LOST., ${computerChoice} Beats Rock`
         }
     }else if(userChoice=="paper"){
         if (computerChoice=="rock"){
+            userScore+=1;
             return "Congrats! You Won., Paper Beats Rock"
         }else{
+            comptScore+=1
             return `SORRY! You LOST., ${computerChoice} Beats Paper`
         }
     }else if(userChoice=="scissors"){
         if (computerChoice=="paper"){
+            userScore+=1;
             return "Congrats! You Won., Scissors Beats Paper!"
         }else{
+            comptScore+=1;
             return `SORRY! You LOST., ${computerChoice} Beats scissors`
         }
     }
 
 }
 
-console.log(gameRound())
+function game(){
+    for (let i =0;i<5;i++){
+        console.log(gameRound())
+    }
+    console.log(`User Score: ${userScore}`)
+    console.log(`Computer Score: ${comptScore}`)
+    userScore>comptScore?console.log("You Won!"):console.log("You LOST!")
+}
+
+// console.log(gameRound())
+game()
